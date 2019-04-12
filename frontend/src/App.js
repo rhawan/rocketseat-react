950d6bf714eac6229c827911bdd4b7842282a526
Rhawan Brenner Silva Franco <rhawan-bs@detran.go.gov.br>
2019-04-12 17:21:28 -0300

    Ajuste projeto backend

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-import Routes from './routes';
-
-const App = () => <Routes />;
-
-export default App;
--- frontend/src/services/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({ baseURL: "https://rockseat-react-backend.herokuapp.com" });
-
-export default api;
